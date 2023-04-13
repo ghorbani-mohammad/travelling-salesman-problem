5d621e36950c67a1f37c796c74680bb0c37c0d7a
Mohammad <mohammad.ghorbani@hotmail.com>
2023-04-14 01:12:21 +0330

    feat: add tsp_solver

--- src/utils/distance.py
+++ src/utils/distance.py
@@ -2,18 +2,18 @@ from typing import List
 from math import sqrt
 
 
-def euclidean_distance(*points: List[float]) -> float:
+def euclidean_distance(*locations: List[float]) -> float:
     """
-    Calculates the Euclidean distance between any number of points.
+    Calculates the Euclidean distance between any number of locations.
     """
-    if len(points) < 2:
+    if len(locations) < 2:
         raise ValueError(
-            "At least two points are required to calculate the Euclidean distance."
+            "At least two locations are required to calculate the Euclidean distance."
         )
     distance = 0
-    for i in range(len(points) - 1):
+    for i in range(len(locations) - 1):
         squared_distance = sum(
-            (p1 - p2) ** 2 for p1, p2 in zip(points[i], points[i + 1])
+            (p1 - p2) ** 2 for p1, p2 in zip(locations[i], locations[i + 1])
         )
         distance += sqrt(squared_distance)
     return distance
